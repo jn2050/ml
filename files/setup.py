ca54a57941826fc3db10ef29992c68725ecf1da4
jn2050 <joao.filipe.neto@gmail.com>
2019-08-24 15:45:48 +0200

    Etc

---
+++ files/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='NN',
+      version='1.0',
+      description='nn lib',
+      author='João Neto',
+      author_email='joao.filipe.neto@gmail.com',
+      packages=['lib'],
+     )
