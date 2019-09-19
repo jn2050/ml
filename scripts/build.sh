990b50c694b3a9685cd23edf8fe68db38ea19e91
jn2050 <joao.filipe.neto@gmail.com>
2019-09-19 14:09:32 +0100

    Etc

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,15 @@
 
-# ml
 #
+# ml
 #
 
 cd ~/dev/proj/ml
 docker build -t ml . # --no-cache
+
+docker tag ml digitallogic/private:ml && \
+    docker push digitallogic/private:ml
+
+docker pull digitallogic/private:ml
 
 # mac jupyter
 docker run -dit \
@@ -16,41 +21,26 @@ docker run -dit \
     ml /bin/bash scripts/ju.sh
 
 # mac sh
-sudo docker run -it --rm \
+docker run -it --rm \
     --privileged \
     -v /Users/jneto/dev:/users/mluser/dev \
     -v /Users/jneto/data:/users/mluser/data \
     ml /bin/bash
 
 
-# cuda1 jupyter
+# cuda1 jupyter (jneto)
 sudo docker run -d \
     --restart unless-stopped \
     --privileged \
     --gpus all \
-    -p 8888:8888 \
+    -p 8000:8888 \
     -v /home/jneto/dev:/users/mluser/dev \
-    -v /dataf:/users/mluser/data \
-    ml /bin/bash scripts/ju.sh
+    -v /dataf:/users/mluser/dev/data \
+    digitallogic/private:ml /bin/bash scripts/ju.sh
 
 # cuda1 sh
 sudo docker run -it --rm \
     --privileged \
     -v /home/jneto/dev:/users/mluser/dev \
     -v /dataf:/users/mluser/data \
-    ml /bin/bash
-
-
-
-docker tag ml digitallogic/private:ml && \
-    docker push digitallogic/private:ml
-
-
-docker pull digitallogic/private:ml
-
-
-# sudo systemctl start docker
-# sudo systemctl stop docker
-
-# sudo docker exec -u 0 -it d3f1cc167bc5 /bin/bash
-# kill 1
+    digitallogic/private:ml /bin/bash
--- scripts/cp.sh
+++ scripts/cp.sh
@@ -1,4 +1,5 @@
 
+# utils
 rm -rf ~/dev/proj/ml/lib/utils
 mkdir ~/dev/proj/ml/lib/utils
 cp ~/dev/proj/lib/python/utils/setup.py ~/dev/proj/ml/lib/utils/setup.py
