f70412f8ddb1bd7109a06980f7ad2af97e617068
João Neto <jneto@MacJN2.local>
2021-04-26 09:53:01 +0100

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ cd ~/dev/lib/ml
 git add . && git commit -m 'update' || git push
 ssh -i ~/.ssh/jn2020 -p 9022 jneto@ml.dlogic.io \
     "cd ~/lib && rm -rf ml && git clone https://github.com/jn2050/ml.git && cd ml &&\
-    sudo docker build -t ml . &&\
+    sudo docker build -t ml . --no-cache &&\
     sudo docker tag ml digitallogic/private:ml &&\
     sudo docker push digitallogic/private:ml" &&\
 ssh -i ~/.ssh/jn2020 -p 9022 jneto@ml.dlogic.io \
