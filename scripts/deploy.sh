4cd44056fccada2bc6ef489df13461b27cf588c1
Joao Neto <joao.filipe.neto@gmail.com>
2021-09-02 17:39:50 +0100

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@
 
 # Build ml image on cuda1
 cd ~/dev/lib/ml
-git add . && git commit -m 'update' || git push
+git add . && git commit -m 'update' && git push
 ssh -i ~/.ssh/jn2020 -p 9022 jneto@ml.dlogic.io \
     "cd ~/lib && rm -rf ml && git clone https://github.com/jn2050/ml.git && cd ml &&\
     sudo docker build -t ml . &&\
